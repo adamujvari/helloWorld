5347cd090ee417b0a77d75284fd9248f2e628cc9
Adam Ujvari <adam.ujvari@outlook.com>
2015-11-25 18:59:25 +0100

    user input parameters

--- assa.c
+++ assa.c
@@ -19,13 +19,13 @@
 #define COMMAND_BUFFER_SIZE 1024
 #define USER_INPUT_PARAM_LENGTH 128
 
-int main(int argc, const char * argv[])
+int main(int argc, const char *argv[])
 {
   char command[COMMAND_BUFFER_SIZE];
-  // char *command_splits = NULL;
-  // char *user_input_parameter_one;
-  // char *user_input_parameter_two;
-  // char *user_input_parameter_three;
+  char *command_splits = "default";
+  char *user_input_parameter_one = "default";
+  char *user_input_parameter_two = "default";
+  char *user_input_parameter_three = "default";
 
   // ckecks parameter count for program mode
 	if (argc == 1)
@@ -43,7 +43,7 @@ int main(int argc, const char * argv[])
   }
 
   // this block handles user input parameters 
-  while (strcmp(command, "quit\n") != 0)
+  while (strcmp(command, "quit") != 0)
   {
     // writes prefix, gets user input and checks for EOF
     printf("esp> ");
@@ -53,25 +53,50 @@ int main(int argc, const char * argv[])
       break;
     }
 
-    // split user input into parameters
-
     // check whether input was an empty line
     if (strcmp(command, "\n") == 0)
     {
       printf("[ERR] wrong parameter count\n");
+
     }
 
+    // split user input into parameters
+    command_splits = strtok(command, " \n");
+    if (command_splits != NULL)
+    {
+      user_input_parameter_one = command_splits;
+      command_splits = strtok(NULL, " \n");
+      if (command_splits != NULL)
+      {
+        user_input_parameter_two = command_splits;
+        command_splits = strtok(NULL, " \n");
+        if (command_splits != NULL)
+        {
+          user_input_parameter_three = command_splits;
+        }
+      }
+    }
+
+    // printf("%s\n", user_input_parameter_one);
+    // printf("%s\n", user_input_parameter_two);
+    // printf("%s\n", user_input_parameter_three);
+
     // run parameter
-    if (strcmp(command, "run\n") == 0)
+    if (strcmp(user_input_parameter_one, "run") == 0)
     {
       // TODO: define bf memory struct + check
       printf("[ERR] no program loaded\n");
     }
     
+    // reset input prameters to default
+    user_input_parameter_one = "default";
+    user_input_parameter_two = "default";
+    user_input_parameter_three = "default";
+    command_splits = "default";
   }
 
   // prints quit
-  if (strcmp(command, "quit\n") == 0)
+  if (strcmp(command, "quit") == 0)
   {
     printf("Bye.\n");
   }
