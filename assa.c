ae0db81c5ec64b5685a57a96866ad58359dca002
adamujvari <adam.ujvari@student.tugraz.at>
2015-11-25 15:32:51 +0100

    sourcecode

---
+++ assa.c
@@ -0,0 +1,82 @@
+//-----------------------------------------------------------------------------
+// assa.c
+//
+// brainfuck interpreter
+//
+// Group: 2 - study assistant Angela Promitzer
+//
+// Authors: Adam Ujvari     1232056
+// 			    Amel Hamidovic  1330013
+//			    Tina Promitzer  1311885
+//
+// Latest Changes: XX.XX.2015
+//-----------------------------------------------------------------------------
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define COMMAND_BUFFER_SIZE 1024
+#define USER_INPUT_PARAM_LENGTH 128
+
+int main(int argc, const char * argv[])
+{
+  char command[COMMAND_BUFFER_SIZE];
+  // char *command_splits = NULL;
+  // char *user_input_parameter_one;
+  // char *user_input_parameter_two;
+  // char *user_input_parameter_three;
+
+  // ckecks parameter count for program mode
+	if (argc == 1)
+	{
+		printf("Interaktive debug mode\n");	
+	}
+	else if (argc == 3)
+	{
+    printf("Program run mode\n");
+	}
+  else
+  {
+    printf("[ERR] usage: ./assa [-e brainfuck_filnename]\n");
+    exit(1);
+  }
+
+  // this block handles user input parameters 
+  while (strcmp(command, "quit\n") != 0)
+  {
+    // writes prefix, gets user input and checks for EOF
+    printf("esp> ");
+    if (fgets(command, COMMAND_BUFFER_SIZE, stdin) == NULL)
+    {
+      printf("\n"); // optional
+      break;
+    }
+
+    // split user input into parameters
+
+    // check whether input was an empty line
+    if (strcmp(command, "\n") == 0)
+    {
+      printf("[ERR] wrong parameter count\n");
+    }
+
+    // run parameter
+    if (strcmp(command, "run\n") == 0)
+    {
+      // TODO: define bf memory struct + check
+      printf("[ERR] no program loaded\n");
+    }
+    
+  }
+
+  // prints quit
+  if (strcmp(command, "quit\n") == 0)
+  {
+    printf("Bye.\n");
+  }
+
+	// TODO: free all the memory!
+
+	return 0;
+}
